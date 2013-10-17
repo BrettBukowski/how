f8540679d58c09a214c4c10cb4a3be8456569037
Brett Bukowski <brett.bukowski@gmail.com>
2013-10-16 21:55:33 -0600

    Retrieve web page, extract links

--- how.go
+++ how.go
@@ -1,18 +1,68 @@
 package main
 
 import (
+	"code.google.com/p/go.net/html"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
 	"os"
 	"strings"
-	// "net/http"
 )
 
-func getResult() {
-
+var userAgents = []string{
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:11.0) Gecko/20100101 Firefox/11.0",
+	"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100 101 Firefox/22.0",
+	"Mozilla/5.0 (Windows NT 6.1; rv:11.0) Gecko/20100101 Firefox/11.0",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_4) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46 Safari/536.5",
+	"Mozilla/5.0 (Windows; Windows NT 6.1) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.46 Safari/536.5",
 }
 
-func searchUrl(https *bool, query string) string {
+func getResult(url string) (string, error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("User-Agent", userAgents[rand.Intn(len(userAgents))])
+
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 200 {
+		body, err := ioutil.ReadAll(resp.Body)
+		return string(body), err
+	}
+	return "An error occurred", err
+}
+
+func extractLinks(numberToExtract *int, htmlDoc string) []string {
+	doc, err := html.Parse(strings.NewReader(htmlDoc))
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+
+	var links []string
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					links = append(links, a.Val)
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+	return links
+}
+
+func searchUrl(https *bool, query []string) string {
+	body := "google.com/search?q=site:stackoverflow.com"
+
 	var pre string
 	if *https {
 		pre = "https://encrypted."
@@ -20,10 +70,7 @@ func searchUrl(https *bool, query string) string {
 		pre = "http://"
 	}
 
-	body := "google.com/search?q=site:stackoverflow.com%20"
-	result := fmt.Sprint(pre, body, query)
-
-	return result
+	return fmt.Sprint(pre, body, "%20", strings.Join(query, "%20"))
 }
 
 func main() {
@@ -35,12 +82,27 @@ func main() {
 
 	if len(flag.Args()) == 0 {
 		flag.PrintDefaults()
+		fmt.Println("Supply a question")
 		os.Exit(2)
 	}
 
-	var url string = searchUrl(https, strings.Join(flag.Args(), "%20"))
+	var url string = searchUrl(https, flag.Args())
 
 	fmt.Println("url: ", url)
-
 	fmt.Println("answers: ", *numAnswers)
+
+	body, err := getResult(url)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	}
+
+	// fmt.Println(body)
+
+	links := extractLinks(numAnswers, body)
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(links[i])
+	}
 }
